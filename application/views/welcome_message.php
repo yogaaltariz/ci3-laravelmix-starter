becc0a42baee60f6854c08d721d5801fc3b0b272
yogaa altariz <yogaaaltariz@pop-os.localdomain>
2021-02-06 21:01:06 +0700

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<link rel="stylesheet" href="<?= base_url('public/css/app.css') ?>">
+</head>
+<body>
+
+<div class="p-8">
+	<h1 class="font-bold text-6xl text-blue-600">Codeigniter 3</h1>
+</div>
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./application/views/**/*.php'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
